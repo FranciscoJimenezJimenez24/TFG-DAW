60a4f460d528b2e841249828d4dd7db2d883325f
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-04 20:59:58 +0100

    change url

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -9,6 +9,6 @@ export class BackendService {
   constructor(private http:HttpClient) { }
 
   signup(data:any){
-    return this.http.post('url',data);
+    return this.http.post('http://127.0.0.1:8000/api/signup',data);
   }
 }
