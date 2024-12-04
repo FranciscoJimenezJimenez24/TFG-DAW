55798d6d23542a59deffb2970f69db2660adb51c
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-04 20:13:08 +0100

    signup catch the data

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  public form={
+    name:null,
+    email:null,
+    password:null,
+    password_confirmation:null
+  }
 
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  sumbitSignup() {
+    console.log(this.form);
+  }
 }
