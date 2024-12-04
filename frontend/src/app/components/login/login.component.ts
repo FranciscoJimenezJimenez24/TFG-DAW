597d308ca39c052c216c03d7a43be65518d22246
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-04 20:04:03 +0100

    login catch the data

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-sumbitLogin() {
-throw new Error('Method not implemented.');
-}
+export class LoginComponent implements OnInit{
+  public form={
+    email:null,
+    password:null
+  }
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  sumbitLogin() {
+    console.log(this.form);
+  }
 
 }
