b99d25105875bc02592e4857595ce8a354bffd6d
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-04 20:26:29 +0100

    backend services signup

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class BackendService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  signup(data:any){
+    return this.http.post('url',data);
+  }
 }
