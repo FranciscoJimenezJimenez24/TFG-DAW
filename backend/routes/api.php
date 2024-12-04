8732394957b9a66d19fa2ee261aa1d8633c773a8
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-04 16:14:51 +0100

    add route group to api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::group(['middleware' => 'api',], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
