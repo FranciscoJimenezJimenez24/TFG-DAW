1607c78d2e0cd21cd96a8747f466bb5c88783a28
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-12-12 14:02:57 +0100

    por fin se arreglo el httpclient

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { provideHttpClient } from '@angular/common/http';
 
 export const routes: Routes = [
     // { path: '', component: HomeComponent }, // Ruta del Home
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
 ];
+export const appConfig = {
+    providers: [provideHttpClient()], // ✅ Proporciona el HttpClient aquí
+    routes: routes // Rutas para tu aplicación
+  };
