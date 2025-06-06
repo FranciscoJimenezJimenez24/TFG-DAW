91818c59b224a7a270de75f27774d5333152497f
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2025-06-06 17:38:40 +0200

    change the loading to 3 seconds

--- frontend/src/app/services/loading.service.ts
+++ frontend/src/app/services/loading.service.ts
@@ -6,14 +6,14 @@ export class LoadingService {
   private loadingSubject = new BehaviorSubject<boolean>(false);
   public loading$ = this.loadingSubject.asObservable();
 
-  private MIN_LOADING_TIME_MS = 5000; // 5 segundos
+  private MIN_LOADING_TIME_MS = 3000; // 3 segundos
 
   // Muestra el spinner
   show() {
     this.loadingSubject.next(true);
   }
 
-  // Oculta el spinner después de 5 segundos (mínimo)
+  // Oculta el spinner después de 3 segundos (mínimo)
   hide() {
     setTimeout(() => {
       this.loadingSubject.next(false);
